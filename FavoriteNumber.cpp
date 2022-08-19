0d255d8a57905778d0c7c0b87d0005bccc0285e0
Ataru2002 <63629987+Ataru2002@users.noreply.github.com>
2022-08-19 20:23:17 +0700

    Create FavoriteNumber.cpp

---
+++ FavoriteNumber.cpp
@@ -0,0 +1,90 @@
+//Link to Problem: https://www.acmicpc.net/problem/10570
+/** Hanabi?
+**                                .
+**                              .'
+**                            .'
+**                            /`-._'
+**                           /   /
+**                          /   /
+**                         /   /
+**                        (`-./
+**                         )
+**                        '       .
+**                              .'
+**                            .'
+**                            /`-._'
+**                           /   /
+**                          /   /
+**                         /   /
+**                        (`-./
+**                         )
+**                        '
+**/
+//Kaicho Shirogane Miyuki
+//Grieveous lady Shinomiya Kaguya
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define alla(a,n) a, a + n
+#define endstring std::string::npos
+#define prtarr(n,len) {for(int i=0;i<len;i++) cout<<n[i]<<" "; cout<<"\n";}
+#define prtset(n) {for(auto i:n) cout<<i<<" "; cout<<"\n";}
+#define prtpair(n) {cout<<n.fr<<" "<<n.sc<<"\n";}
+#define judge(a) {if(a) cout<<"YES"<<"\n"; else cout<<"NO"<<"\n";}
+#define search(a,n) a.find(n)
+#define Ataru main
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<pll> vpll;
+typedef vector<vll> vvll;
+typedef vector<vi> vvii;
+typedef vector<string> vs;
+int inquire(int x,int y){
+    cout<<"? "<<x<<" "<<y<<endl;
+    int a; cin>>a;
+    return a;
+}
+const ll INF= 0x3f3f3f3f;
+const ll MAX=1e5+1;
+
+
+int n, v, s;
+vi lists;
+int Ataru() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL) ; cout.tie(NULL) ;
+    //std::cout<<std::setprecision(3)<<std::fixed;
+    cin >> n;
+    while(n--){
+        cin >> v;
+        lists.resize(v);
+        vi cnt(1001, 0);
+        for(int i = 0; i < v; i++){
+            cin >> s;
+            cnt[s]++;
+        }
+        int mx = 0;
+        int mxn = -1;
+        for(int i = 1; i < 1001; i++){
+            if(mx < cnt[i]){
+                mx = cnt[i];
+                mxn = i;
+            }
+        }
+        cout << mxn << "\n";
+    }
+    return 0;
+}
